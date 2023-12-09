3df66f23deb9edddba9e4a97cfd8c5ac8331a5dc
Austin Lynes <austinlynes94@gmail.com>
2023-12-09 09:12:28 -0800

    added build config for premake

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "stb"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("../Build/" .. OutputDir .. "/%{prj.name}")
+	objdir ("../Build/Intermediates/" .. OutputDir .. "/%{prj.name}")
+	
+	files
+	{
+        "stb_image.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
